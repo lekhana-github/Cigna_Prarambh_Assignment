9ec7528b2b6a7b4cae56bd1daab223fabc8daa47
lekhana-github <141510737+lekhana-github@users.noreply.github.com>
2025-11-04 15:29:31 +0530

    Create Logger.cs

---
+++ 04.Week-04/02.Day16/Logger.cs
@@ -0,0 +1,47 @@
+// 3.     Create a C# program to implement the following requirement:
+
+// 		a.   You have a Logger class that writes messages to the console.
+// 		b.  Create multiple overloaded methods for different use cases (basic message, with level, with timestamp).
+// 		c.   Level :   may be INFO, WARNING, ERROR 
+
+
+using System.Security;
+
+namespace Day16
+
+{
+    internal class Logger
+
+    {
+        // Overloaded methods
+
+        static void Log(string message)
+        {
+            Console.WriteLine($"[MESSAGE]: {message}");
+        }
+
+        static void Log(string message, string level)
+        {
+            Console.WriteLine($"[{level.ToUpper()}]: {message}");
+        }
+
+        static void Log(string message, string level, bool withTimestamp)
+        {
+            string timestamp = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+            if (withTimestamp)
+                Console.WriteLine($"[{timestamp}] [{level.ToUpper()}]: {message}");
+            else
+                Console.WriteLine($"[{level.ToUpper()}]: {message}");
+        }
+        static void Main(string[] args)
+        {
+
+            Log("Application started");
+            Log("Low disk space", "WARNING");
+            Log("File not found", "ERROR", true);
+
+        }
+
+    }
+
+}
