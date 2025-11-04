0ec53df71b51d3345bda30e9b63eb1d2565056c3
lekhana-github <141510737+lekhana-github@users.noreply.github.com>
2025-11-04 14:39:13 +0530

    Create 02.Day16.cs

---
+++ 04.Week-04/02.Day16.cs
@@ -0,0 +1,35 @@
+using System.Security;
+
+namespace Email
+{
+    internal class Program
+    {
+        // Method with optional parameters
+        static void SendEmail(
+            string sender,
+            string recipient,
+            string subject = "No Subject",
+            string message = "No Message",
+            bool isImportant = false)
+        {
+            Console.WriteLine("----- Email Details -----");
+            Console.WriteLine($"From: {sender}");
+            Console.WriteLine($"To: {recipient}");
+            Console.WriteLine($"Subject: {subject}");
+            Console.WriteLine($"Message: {message}");
+            Console.WriteLine($"Important: {(isImportant ? "Yes" : "No")}");
+            Console.WriteLine("-------------------------\n");
+        }
+        static void Main(string[] args)
+        {
+            // Calling with all parameters
+            SendEmail("lekhanak@gmail.com", "sanju@gmail.com.com", "Meeting", "Let's meet at 5 \n PM", true);
+
+            // Calling with only required parameters
+            SendEmail("anu@example.com", "manu@example.com");
+
+            // Calling with some optional ones
+            SendEmail("sanu@example.com", "sonu@example.com", message: "Good morning!");
+        }
+    }
+}
